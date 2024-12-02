5ce87012a869b1d8e0f26c6afd1e672a6474abc1
roncelli.23346 <roncelli.leonardo.studente@itispaleocapa.it>
2024-12-02 12:41:50 +0100

    Attributi e metodi set get della classe Agenzia.

--- src/Agenzia.java
+++ src/Agenzia.java
@@ -1,4 +1,23 @@
 public class Agenzia {
-
-
+    private String destinazione;
+    private int numGiorni;
+    private double prezzoBase;
+    public void setDestinazione(String destinazione){
+        this.destinazione=destinazione;
+    }
+    public String getDestinazione(){
+        return destinazione;
+    }
+    public void setNumGiorni(int numGiorni){
+        this.numGiorni=numGiorni;
+    }
+    public int getNumGiorni(){
+        return numGiorni;
+    }
+    public void setPrezzoBase(double prezzoBase){
+        this.prezzoBase=prezzoBase;
+    }
+    public double getPrezzoBase(){
+        return prezzoBase;
+    }
 }
