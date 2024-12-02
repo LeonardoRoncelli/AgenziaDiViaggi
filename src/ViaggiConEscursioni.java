fbbd3a53d83c8ea9cb213d2e8df19ead226d5e13
roncelli.23346 <roncelli.leonardo.studente@itispaleocapa.it>
2024-12-02 13:36:26 +0100

    Metodi set, get e toString della classe ViaggiConEscursioni.

--- src/ViaggiConEscursioni.java
+++ src/ViaggiConEscursioni.java
@@ -1,2 +1,16 @@
-public class ViaggiConEscursioni {
+public class ViaggiConEscursioni extends Agenzia{
+    private int numEscursioni;
+    public ViaggiConEscursioni(String destinazione,int numGiorni,double prezzoBase,int numEscursioni){
+        super(destinazione,numGiorni,prezzoBase);
+        this.numEscursioni =numEscursioni;
+    }
+    public void setNumEscursioni(int numEscursioni){
+        this.numEscursioni=numEscursioni;
+    }
+    public int getNumEscursioni(){
+        return numEscursioni;
+    }
+    public String toString(){
+        return ("Numero delle escursini: "+getNumEscursioni());
+    }
 }
